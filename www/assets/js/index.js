b533615e110db872401a0801accdd9fb8b23def3
st55448 <st55448@upce.cz>
2021-08-19 20:54:22 +0200

    Login page refactor

--- www/assets/js/index.js
+++ www/assets/js/index.js
@@ -1,9 +1,50 @@
-/*---------------------------------------------*/
-$(document).ready(function(){
-  $('.alert').alert();
+$(document).ready(function () {
+    $(".alert").hide(); //Hide error message
+
+    $(document).bind('keypress', function (e) {
+        if (e.keyCode == 13) {
+            $('#loginBtn').trigger('click');
+        }
+    });// enter key
+
+    $("#loginBtn").click(function () {
+        var credentials = {username: $("#username").val(), password: $("#password").val()};
+        if (!credentials.username || !credentials.password) {
+            showError("Uživatelské jméno či heslo nebylo vyplněno");
+            return;
+        } //validate input if not empty
+
+        $.ajax({
+            url: SERVER + '/login',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(credentials) //Send creds to rest api
+        }).done(function (response, status, xhr) {
+            var authToken = xhr.getResponseHeader("Authorization");
+            console.log(xhr.status);
+            document.cookie = "Authorization=" + authToken;
+            window.location.replace("administration.html"); //Set Authorization header to cookies if login success
+        }).fail(function (xhr, status) {
+            if (!xhr) {
+                showError("Server neodpovídá");
+                return;
+            }
+
+            if (xhr.status === 403) {
+                showError("Vaše přihlašovací údaje nejsou správné")
+                $("#username").val("");
+                $("#password").val("");
+            }
+        })
+    });
+
+    $('.alert .close').on('click', function (e) {
+        $(this).parent().hide();
+    }); //Hide alert about error
 });
 
-/*---------------------------------------------*/
-$(window).on('load', function(){
-
-});
+function showError(text) {
+    $("#errorText").text(text);
+    $(".alert").addClass("show");
+    $(".alert").show(); //Show error
+}
